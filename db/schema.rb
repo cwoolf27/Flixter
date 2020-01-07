32efaa81f7072bde9a37b9b49b1fe447c4f28e5c
Cactus Woolf <cactuswoolf@gmail.com>
2020-01-07 16:18:52 +0000

    migrations p2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_07_152404) do
+ActiveRecord::Schema.define(version: 2020_01_07_161730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_01_07_152404) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "video"
+    t.string "image"
     t.index ["section_id"], name: "index_lessons_on_section_id"
   end
 
