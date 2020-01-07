368ee25fce7d29dd4e996f83407fa7c97a9a9853
Cactus Woolf <cactuswoolf@gmail.com>
2020-01-07 21:02:48 +0000

    enroll authorize

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,17 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson.section.course, only: [:show]
+
   def show
   end
 
   private
+
+  def require_authorized_for_current_lesson
+    if current_course.user != current_user
+      redirect_to courses_path, alert: 'Error, Must Be Enrolled'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
