f58719c7599698d49f5d3368a7a6851274b534ee
Cactus Woolf <cactuswoolf@gmail.com>
2020-01-08 15:23:18 +0000

    perms

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :courses
   has_many :enrollments
   has_many :enrolled_courses, through: :enrollments, source: :course
+  has_many :lessons
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -11,5 +12,5 @@ class User < ApplicationRecord
   def enrolled_in?(course)
     return enrolled_courses.include?(course)
   end
-  
+
 end
