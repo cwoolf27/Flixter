cd444c933f9f45c7d2146c88eff9205572215815
Cactus Woolf <cactuswoolf@gmail.com>
2020-01-09 15:40:11 +0000

    ranking lessons

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_07_172937) do
+ActiveRecord::Schema.define(version: 2020_01_09_152555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2020_01_07_172937) do
     t.datetime "updated_at", null: false
     t.string "video"
     t.string "image"
+    t.integer "row_order"
+    t.index ["row_order"], name: "index_lessons_on_row_order"
     t.index ["section_id"], name: "index_lessons_on_section_id"
   end
 
